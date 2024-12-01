cccb2d9bf815d3d602041c188427aa1a09276204
Flivian91 <fliviandev@gmail.com>
2024-12-01 16:37:08 +0300

    commit

--- src/components/settings/SettingsAreaDisplay.jsx
+++ src/components/settings/SettingsAreaDisplay.jsx
@@ -5,7 +5,7 @@ import SettingsNavigationLinnks from "./SettingsNavigationLinnks";
 function SettingsAreaDisplay({ children }) {
   return (
     <div className="flex flex-col gap-2 relative ">
-      <div className="w-4/5 absolute -top-7 left-1/2 transform -translate-x-1/2 bg-surface px-2">
+      <div className="w-4/5 absolute -top-7 left-1/2 transform -translate-x-1/2 bg-surface px-2 rounded shadow">
         <SettingsHeader />
         <SettingsNavigationLinnks />
         <div>{children}</div>
--- src/components/settings/SettingsNavigationLinnks.jsx
+++ src/components/settings/SettingsNavigationLinnks.jsx
@@ -1,11 +1,39 @@
+import Link from "next/link";
 import React from "react";
 
 function SettingsNavigationLinnks() {
   return (
-    <div className="mt-14 border-b border-text/30">
-      <div></div>
+    <div className="mt-12 border-b border-text/30 flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <Link
+          href={"/dashboard/settings"}
+          className="px-2 py-2 border-b-2 text-sm font-bold tracking-wide border-accent hover:text-accent transition-all duration-300"
+        >
+          Profile
+        </Link>
+        <Link
+          href={"/dashboard/settings/password"}
+          className="px-2 py-2 border-b-2 border-transparent text-sm font-bold tracking-wide hover:text-accent transition-all duration-300"
+        >
+          Change Password
+        </Link>
+        <Link
+          href={"/dashboard/settings/email"}
+          className="px-2 py-2 border-b-2 border-transparent text-sm font-bold tracking-wide hover:text-accent transition-all duration-300"
+        >
+          Update Email
+        </Link>
+        <Link
+          href={"/dashboard/settings/notifications"}
+          className="px-2 py-2 border-b-2 border-transparent text-sm font-bold tracking-wide hover:text-accent transition-all duration-300"
+        >
+          Notifications
+        </Link>
+      </div>
       <div>
-        <button>Delete Account</button>
+        <button className="text-sm font-bold text-red-500 tracking-wide">
+          Delete Account
+        </button>
       </div>
     </div>
   );
