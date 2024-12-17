2cf0409ca1e6c3aee4adf6a05a17930d55fb246f
Flivian91 <fliviandev@gmail.com>
2024-12-17 21:08:08 +0300

    commit

--- src/components/header/ProfileContainer.jsx
+++ src/components/header/ProfileContainer.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import DownloadButton from "./DownloadButton";
 import Profile from "./Profile";
 import { BiSearch } from "react-icons/bi";
 import SearchModel from "../models/SearchModel";
-import SearchOvelay from "../ovelays/SearchOvelay";
 
 function ProfileContainer() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="flex items-center gap-4">
       <div className="hidden md:flex">
         <DownloadButton />
       </div>
-      <SearchModel />
-      <button className="md:hidden block p-1 sm:p-1.5 text-xl sm:text-2xl rounded shadow text-white/70 hover:text-accent active:text-accent">
+      {isOpen && <SearchModel />}
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="md:hidden block p-1 sm:p-1.5 text-xl sm:text-2xl rounded shadow text-white/70 hover:text-accent active:text-accent"
+      >
         <BiSearch />
       </button>
       <Profile />
